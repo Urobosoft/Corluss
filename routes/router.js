ad8c8f37384fe21c934d9eb9f8a70c1eb57e07fa
Urobosoft <quantum.rumblers079@gmail.com>
2023-11-10 19:11:04 -0600

    path1

--- routes/router.js
+++ routes/router.js
@@ -4,35 +4,33 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const upload = authController.upload;
 
-// Asumiendo que tu aplicación se sirve bajo el prefijo '/Corluss'
-
 // Ruta para la página "home" (mostrada antes de "index")
-router.get('/Corluss/', (req, res) => {
+router.get('/', (req, res) => {
     res.render('index');
 });
 
-// Ruta para la página "home"
-router.get('/Corluss/home', authController.isAuthenticated, (req, res) => {
+// Ruta para la página "index"
+router.get('/home', authController.isAuthenticated, (req, res) => {
     if (req.user) {
         res.render('home', { user: req.user }); // Pasa los datos del usuario a la vista "home"
     } else {
-        res.redirect('/Corluss/login');
+        res.redirect('/login');
     }
 });
-
 // Rutas para el login y registro
-router.get('/Corluss/login', (req, res) => {
+router.get('/login', (req, res) => {
     res.render('login', { alert: false });
 });
 
-router.get('/Corluss/registro', (req, res) => {
+router.get('/registro', (req, res) => {
     res.render('registro', { alert: false });
 });
 
+
 // Rutas para los métodos del controlador
 // Ruta para el registro
-router.post('/Corluss/register', upload.single('image'), authController.register);
-router.post('/Corluss/login', authController.login);
-router.get('/Corluss/logout', authController.logout);
+router.post('/register', upload.single('image'), authController.register);
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 
 module.exports = router;
