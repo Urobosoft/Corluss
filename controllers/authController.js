2e65df849fa09f5b69559ceca41e66252dd781af
Your Name <you@example.com>
2023-11-30 09:09:11 -0600

    RUTAS

--- controllers/authController.js
+++ controllers/authController.js
@@ -656,7 +656,7 @@ exports.login = async (req, res) => {
         };
 
         if (!nomUsuario || !password) {
-            res.render('/Corluss/login', {
+            return res.render('/Corluss/login', {
                 alert: true,
                 alertTitle: "Advertencia",
                 alertMessage: "Ingrese un usuario y password",
